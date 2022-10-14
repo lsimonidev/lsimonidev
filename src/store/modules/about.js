8bb0d01fe2785ad84cd5c6d1ab368dfeeb3dfd7b
Laura Simoni <laura.simoni@gmail.com>
2022-10-14 16:57:51 +0100

    Update about.js

--- src/store/modules/about.js
+++ src/store/modules/about.js
@@ -3,7 +3,7 @@ const state = {
     myName: 'Laura Simoni',
     greeting: "Hi. I'm",
     intro:
-      "I'm a brasilian web developer, based in Porto (PT), who loves cats, music and mechanical keyboards. I believe that developing software is a great way to help people while keeping an active mind.",
+      "I'm a brazilian web developer, based in Porto (PT), who loves cats, music and mechanical keyboards. I believe that developing software is a great way to help people while keeping an active mind.",
   },
 };
 
